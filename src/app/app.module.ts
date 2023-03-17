f1240529b7a0957e12878393ac33dd8bfd5b998c
Ali tebbal <matricule@cstj.qc.ca>
2023-03-17 15:05:02 -0400

    travail 5-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemeComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path:'accueil', component:AccueilComponent},
+      { path:'probleme', component:ProblemeComponent},    
+      { path:'', redirectTo:'accueil', pathMatch:'full'},
+      { path:'**', redirectTo:'accueil', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
